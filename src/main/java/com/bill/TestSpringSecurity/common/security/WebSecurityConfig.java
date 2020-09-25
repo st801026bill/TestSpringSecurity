47439ea3c1d6afe5e6c9458c0d4698d4cd2f2dc7
Bill_Chou <bill.chou@tpisoftware.com>
2020-09-25 15:35:39 +0800

    由db控管操作權限

--- src/main/java/com/bill/TestSpringSecurity/common/security/WebSecurityConfig.java
+++ src/main/java/com/bill/TestSpringSecurity/common/security/WebSecurityConfig.java
@@ -4,6 +4,9 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -11,7 +14,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -20,47 +23,53 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
+	
+	@Autowired
+	DataSource datasource;
+	
 	@Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+//        return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
+	}
 
 	 @Bean
-     LoginAuthenticationFilter loginAuthenticationFilter() throws Exception {
-         LoginAuthenticationFilter filter = new LoginAuthenticationFilter();
-         filter.setAuthenticationManager(authenticationManagerBean());
-         filter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandlerImpl());
-         filter.setAuthenticationFailureHandler(new AuthenticationFailureHandlerImpl());
-         filter.setFilterProcessesUrl("/api/login");
-         return filter;
+	 LoginAuthenticationFilter loginAuthenticationFilter() throws Exception {
+	     LoginAuthenticationFilter filter = new LoginAuthenticationFilter();
+	     filter.setAuthenticationManager(authenticationManagerBean());
+	     filter.setAuthenticationSuccessHandler(new AuthenticationSuccessHandlerImpl());
+	     filter.setAuthenticationFailureHandler(new AuthenticationFailureHandlerImpl());
+	     filter.setFilterProcessesUrl("/api/login");
+	     return filter;
 	 }
 	 
 	 @Override
-     @Bean
-     public AuthenticationManager authenticationManagerBean() throws Exception {
-         return super.authenticationManagerBean();
-     }
+	 @Bean
+	 public AuthenticationManager authenticationManagerBean() throws Exception {
+	     return super.authenticationManagerBean();
+	 }
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// TODO Auto-generated method stub
-		auth.inMemoryAuthentication()
-	        .withUser("admin").password("123456").roles("ADMIN").and()
-	        .withUser("user").password("123456").roles("USER");
+//		auth.inMemoryAuthentication()
+//	        .withUser("admin").password("123456").roles("ADMIN").and()
+//	        .withUser("user").password("123456").roles("USER");
+		auth.jdbcAuthentication().dataSource(datasource).passwordEncoder(passwordEncoder());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.exceptionHandling()
-	        .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-	        .accessDeniedHandler(new AccessDeniedHandlerImpl())
+	        .authenticationEntryPoint(new RestAuthenticationEntryPoint())	//判斷是否登入
+	        .accessDeniedHandler(new AccessDeniedHandlerImpl())				//判斷權限
         .and()
 	        .addFilterAt(loginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 	        .authorizeRequests()
 	        .antMatchers("/api/admin/**").hasRole("ADMIN")
 	        .antMatchers("/api/user/**").hasRole("USER")
+	        .anyRequest().permitAll()
         .and()
 	        .logout()
 	        .logoutUrl("/api/logout")
